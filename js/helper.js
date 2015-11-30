5dab53cb2a966ae459efa29285f7a3eb73691017
baharman78 <baharman78@users.noreply.github.com>
2015-11-30 16:01:09 -0500

    Removed hello world from index.html Added four objects for defining information in resumeBuilder.js Added = to line 192 statement in helper.js after running jshint

--- js/helper.js
+++ js/helper.js
@@ -65,7 +65,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
@@ -109,9 +109,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -189,7 +189,7 @@ function initializeMap() {
   If so, it creates a new map marker for that location.
   */
   function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
     }
   }
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,113 @@
+var bio = {
+  "name" : "Brent Harman",
+  "role" : "Software Engineer",
+  "contact" : {
+    "email" : "brent.a.harman@gmail.com",
+    "mobile" : "574-527-4733",
+    "github" : "baharman78",
+    "location" : "Warsaw, IN"
+  },
+  "picture_URL" : "images/fry.jpg",
+  "welcome_message" : "Welcome",
+  "skills" : ["Python, Java, C, C++, VHDL, Visual Basic, and Assembly programming languages",
+  "Autocad, Modelsim, PSpice, Microsoft Office, and Matlab software",
+  "Microcontrollers, FPGA's, and PLD's",
+  "PLC 5's SLC 500's, Micrologix, CompactLogix, and ControlLogix",
+  "Rslogix 500, Rslogix 500, Rslogix 5",
+  "Devicenet, Controlnet, and Ethernet I/O",
+  "FactoryTalk SE, FactoryTalk ME, Panelviews, and Thinmanager",
+  "Powerflex 70 VFDs, Powerflex 525 VFD's, Allen Bradley 160 VFDs, and Armorstart VFDs"
+  ]
+};
+
+var work = {
+  "employer" : "Red Gold",
+  "title" : "Controls Technician",
+  "workdates" : "August 2014 - July 2015",
+  "worklocation" : "Geneva, IN",
+  "workdescription" : [
+    "Provided controls support in a manufacturing facility",
+    "Maintained Allen Bradley PLC's, ACP thin clients, VFD Drives, Panelview, and other hardware for end user",
+    "Implemented software and hardware changes depending on what operators, supervisors, and managers required",
+    "Trained controls electricians after controls issues were solved",
+    "Delegated small controls projects for electricians to do electrical work on",
+    "Provided remote on-call support after business hours"
+  ]
+};
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Purdue University",
+      "degree" : "BA",
+      "dates" : "December 2013",
+      "location" : "West Lafayette, Indiana",
+      "major" : "Computer Engineering"
+    }
+  ],
+  "certificates" : [
+    {
+      "name" : "Udacity",
+      "degree" : "nanodegree",
+      "dates" : "Present",
+      "location" : "Online",
+      "major" : "Front End Web Developer"
+    }
+  ]
+};
+
+var projects = [
+    {
+      "title" : "Data Structures",
+      "dates" : "Fall 2010",
+      "description" : [
+        "Implemented a zipping and unzipping program that utilized various data structures with a partner using C",
+        "Created a recursive function to unzip data in order to preserve the pattern utilizing a binary search tree",
+        "Learned that data structures provide different run times depending on which ones are used"
+      ],
+      "project_img" : "images/fry.jpg"
+    },
+    {
+      "title" : "Senior Design",
+      "dates" : "Fall 2013",
+      "description" : [
+        "Designed automation subsystem for a 3 axis pick and place system that utilizes stepper motors and an electro-magnet to pick up randomly placed objects on a grid layout and return them to a home location on the grid",
+        "Presented design review with team and individually in order to show the progress periodically throughout project",
+        "Incorporated modularization in project to allow for easy integration of future subsystems"
+      ],
+      "project_img" : "images/fry.jpg"
+    },
+    {
+      "title" : "Vertically Integrated Projects",
+      "dates" : "Spring 2013",
+      "description" : [
+        "Collaborated with grad students on software for a football telemetry helmet circuit prototype involving a microcontroller in order to collect data on collisions during football games",
+        "Programmed microcontroller to work with gyroscopes and accelerometer to collect and store data",
+        "Started interfacing individual sensors with microcontroller, and then considered how to incorporate additional sensors afterward"
+      ],
+      "project_img" : "images/fry.jpg"
+    },
+    {
+      "title" : "Microcontrollers",
+      "dates" : "Spring 2012",
+      "description" : [
+        "Participated with peers in the design of a Bop-It type system using a microcontroller and electrical components",
+        "Designed embedded circuits to interface with microcontroller and then soldiered the final product together",
+        "Utilized interrupts and polling in order to receive data from all of the electrical components"
+      ],
+      "project_img" : "images/fry.jpg"
+      },
+      {
+        "title" : "Computer Architecture",
+        "dates" : "Spring 2013",
+        "description" : [
+          "Designed a multicore pipelined processor with a partner using VHDL",
+          "Implemented optimization techniques for speeding up processor by using probability",
+          "Completed smaller projects that built upon each other to create the final project"
+        ],
+        "project_img" : "images/fry.jpg"
+  }
+];
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
